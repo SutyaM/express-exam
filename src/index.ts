95cc7a2f9468c0b93a6f3eb597011a366936db9a
SutyaM <sutya.miklos.cv@gmail.com>
2020-05-11 09:59:19 +0200

    last commit

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,13 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
-import * as createMiddleware from 'swagger-express-middleware';
-import { SwaggerMiddleware } from 'swagger-express-middleware';
-import { database } from './lib/database';
 import { router } from './app/routers/flat';
 
 const app = express();
 app.use(express.json());
 const { PORT = 3000 } = process.env;
 
-createMiddleware('config/swagger.json', app, (err, middleware: SwaggerMiddleware) => {
-  if (err) {
-    console.error(err);
-  }
-
-  app.use(
-    middleware.metadata(),
-    middleware.CORS(),
-    middleware.parseRequest(),
-    middleware.validateRequest()
-  );
 
   app.use(router);
 
   app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT}`);
   });
-});
